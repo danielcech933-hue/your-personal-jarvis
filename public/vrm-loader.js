12c3b53b8c504662c58cb513998e4c2336f1f6f6
danielcech933-hue <danielcech933@gmail.com>
2026-09-15 13:01:16 +0200

    Make VRM loader fail-fast and observable

--- public/vrm-loader.js
+++ public/vrm-loader.js
@@ -1,12 +1,19 @@
+let jarvisVrmLoading = true;
+
 try {
   const THREE = await import("three");
   const { GLTFLoader } = await import("three/addons/loaders/GLTFLoader.js");
   const { VRMLoaderPlugin, VRMUtils } = await import("@pixiv/three-vrm");
+
   window.THREE = THREE;
   window.THREE_GLTFLoader = GLTFLoader;
   window.THREE_VRM = { VRMLoaderPlugin, VRMUtils };
+  jarvisVrmLoading = false;
   window.dispatchEvent(new Event("jarvis-vrm-ready"));
 } catch (error) {
+  jarvisVrmLoading = false;
   console.error("JARVIS VRM loader failed", error);
   window.dispatchEvent(new CustomEvent("jarvis-vrm-error", { detail: String(error) }));
 }
+
+export { jarvisVrmLoading };
